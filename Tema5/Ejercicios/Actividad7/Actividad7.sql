2b994bec0909d6288c66ab90ae26f1236d2c2cf3
José Manuel <71851066+Joseman2000LR@users.noreply.github.com>
2024-02-15 20:19:09 +0000

    creacion de activiadad7

---
+++ Tema5/Ejercicios/Actividad7/Actividad7.sql
@@ -0,0 +1,93 @@
+PASO 2: LECTURA DEL FICHERO SQL.
+
+1. Entra en SQLite3 a través del siguiente comando:
+
+SQLITE3 TAREA7.DB
+
+2. Haciendo un .read del fichero sql, de nombre supermercado-dump.sql, realiza la
+
+
+/**1. Mostrar todos los productos de la categoría "Bebidas".**/
+
+/**2. Listar los productos ordenados por precio de forma descendente.**
+/
+
+/**3. Calcular el precio total de todos los productos en la tabla "productos".**
+/
+
+/**4. Encontrar los productos con un nombre que contenga la letra 'a'.**
+/
+
+/**5. Obtener la cantidad total de productos vendidos en todas las fechas.**
+/
+
+/**6. Encontrar el producto más caro en cada categoría.**
+/
+
+/**7. Listar los productos que no han sido vendidos.**
+/
+
+/**8. Calcular el precio promedio de los productos en la categoría "Snacks".**
+/
+
+/**9. Encontrar los productos que han sido vendidos más de 5 veces.**
+/
+
+/**10. Mostrar la fecha y la cantidad de ventas para cada producto.**
+/
+
+/**11. Encontrar los productos que tienen un precio menor o igual a 2.**
+/
+
+/**12. Calcular la cantidad total de ventas para cada fecha.**
+/
+
+/**13. Listar los productos cuyo nombre comienza con la letra 'P'.**
+/
+
+/**14. Obtener el producto más vendido en términos de cantidad.**
+/
+
+/**15. Mostrar los productos que fueron vendidos en la fecha '2024-01-18'.**
+/
+
+/**16. Calcular el total de ventas para cada producto.**
+/
+
+/**17. Encontrar los productos con un precio entre 3 y 4.**
+/
+
+/**18. Listar los productos y sus categorías ordenados alfabéticamente por categoría.**
+/
+
+/**19. Calcular el precio total de los productos vendidos en la fecha '2024-01-19'.**
+/
+
+/**20. - Mostrar los productos que no pertenecen a la categoría "Higiene".**
+/
+
+/**21. - Encontrar la cantidad total de productos en cada categoría.**
+/
+
+/**22. - Listar los productos que tienen un precio igual a la media de precios.**
+/
+
+/**23. - Calcular el precio total de los productos vendidos en cada fecha.**
+/
+
+/**24. - Mostrar los productos con un nombre que termina con la letra 'o'.**
+/
+
+/**25. - Encontrar los productos que han sido vendidos en más de una fecha.**
+/
+
+/**26. - Listar los productos cuya categoría comienza con la letra 'L'.**
+/
+
+/**27. - Calcular el total de ventas para cada producto en la fecha '2024-01-17'.**
+/
+
+/**28. - Mostrar los productos cuyo nombre tiene al menos 5 caracteres.**
+/
+
+/**29. - Encontrar los productos que tienen un precio superior al precio máximo en la tabla "productos".**/
