4d1c7ea0f8f6a24694b934baf2003c817dafa154
José Manuel <71851066+Joseman2000LR@users.noreply.github.com>
2024-02-21 13:23:23 +0000

    creacion Actividad 8

---
+++ Tema5/Ejercicios/Actividad8/Tarea8.sql
@@ -0,0 +1,87 @@
+/**1.SELECCIÓN DE VIDEOJUEGOS CUYO TÍTULO COMIENZA CON "C".**/
+
+/**2.VIDEOJUEGOS CREADOS POR DESARROLLADORAS CUYOS NOMBRES TERMINAN CON "s".**/
+
+/**3.VIDEOJUEGOS CON TÍTULOS QUE CONTIENEN LA PALABRA "and" EN CUALQUIER POSICIÓN.**/
+
+/**4.VIDEOJUEGOS CUYO TÍTULO COMIENZA CON UNA VOCAL.**
+/
+
+/**5.VIDEOJUEGOS CUYA DESARROLLADORA TIENE AL MENOS UNA VOCAL REPETIDA.**
+/
+
+/**6.VIDEOJUEGOS CON PRECIOS QUE TIENEN DOS DÍGITOS DECIMALES EXACTOS.**
+/
+
+/**7.VIDEOJUEGOS CUYOS TÍTULOS TIENEN AL MENOS TRES PALABRAS.**
+/
+
+/**8. Obtener todos los desarrolladoras cuyo nombre empieza con la letra "N".**
+/
+
+/**9. Seleccionar los videojuegos cuyo título contiene la palabra "Witcher".**
+/
+
+/**10. Obtener todas las desarrolladoras cuyo nombre termina con "ez".**
+/
+
+/**11. Obtener todos las desarrolladoras cuyo nombre tiene al menos 7 caracteres.**
+/
+
+/**12. Seleccionar los videojuegos cuya desarrolladora es diferente de "Ubisoft".**
+/
+
+/**13. Obtener todos las desarrolladoras cuyo nombre contiene al menos una vocal.**
+/
+
+/**14. Seleccionar los videojuegos cuyo título comienza con una letra mayúscula.**
+/
+
+/**15. Obtener todas las desarrolladoras cuyo nombre no contiene la letra "r".**
+/
+
+/**16. Seleccionar los videojuegos cuya desarrolladora empieza con la letra "B".**
+/
+
+/**17. Obtener todas las desarrolladoras cuyo nombre tiene exactamente 6 caracteres.**
+/
+
+/**18. Seleccionar los videojuegos cuyo título contiene al menos un número.**
+/
+
+/**19. Obtener todas las desarrolladoras cuyo nombre inicia con una vocal.**
+/
+
+/**20. Obtener todas las desarrolladoras cuyo nombre no contiene espacios en blanco.**
+/
+
+/**21. Seleccionar los videojuegos cuyo título termina con una vocal.**
+/
+
+/**22. Obtener todas las desarrolladoras cuyo nombre contiene la secuencia "ar".**/
+
+/**23. Seleccionar los videojuegos cuyo título empieza con la palabra "The".**
+/
+
+/**24. Obtener todas las desarrolladoras cuyo nombre tiene al menos una letra mayúscula.**
+/
+
+/**25. Obtener todos los videojuegos cuyo nombre no contiene números.**
+/
+
+/**26. Seleccionar los videojuegos cuyo título contiene al menos tres vocales.**
+/
+
+/**27. Obtener todas las desarrolladoras cuyo nombre inicia con una consonante.**
+/
+
+/**28. Seleccionar los videojuegos cuyo título no contiene la palabra "The".**
+/
+
+/**29. Obtener todas las desarrolladoras cuyo nombre tiene al menos una letra repetida CONSECUTIVAMENTE.**
+/
+
+/**30. Obtener todas las desarrolladoras cuyo nombre empieza con "U" o termina con "x":**
+/
+
+/**31. Obtener todas las desarrolladoras cuyo nombre no contiene caracteres especiales.**/
